4b52348f18072b89ea031fbc2419c2a39d0da7d2
harry <harry.makins@gmail.com>
2021-03-04 09:30:21 +0000

    boiler plate upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+
+const mainContainer = document.querySelector(".main-container");
+
+function makeGrid(row, column) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+
+    main - container.appendChild(gridItem);
+}
